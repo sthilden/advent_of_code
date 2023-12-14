0cc31e91c2a7185be6d85e53d1215cc4209760db
Sindre Hilden <sindre.t.hilden@gmail.com>
2023-12-14 14:28:41 +0100

    Day 13, problem 2

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod year2023;
 use crate::year2023::*;
 
 fn main() {
-    day13::problem1();
+    day13::problem2();
 }
--- src/year2023/day13.rs
+++ src/year2023/day13.rs
@@ -6,7 +6,7 @@ enum Item {
     Rock,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Pattern {
     rows: Vec<Vec<Item>>,
     columns: Vec<Vec<Item>>,
@@ -21,9 +21,15 @@ impl Pattern {
     }
 }
 
-fn find_symmtery_one_dimension(items: &Vec<Vec<Item>>) -> usize {
+fn find_symmtery_one_dimension(items: &Vec<Vec<Item>>, avoid: Option<usize>) -> usize {
     let mut split_size = 0;
     for split_inx in 0..items.len() - 1 {
+        if let Some(avoid_inx) = avoid {
+            if avoid_inx == split_inx {
+                continue;
+            }
+        }
+
         // Split between split_inx and split_inx + 1
         let mut left_inx = split_inx;
         let mut right_inx = split_inx + 1;
@@ -43,20 +49,61 @@ fn find_symmtery_one_dimension(items: &Vec<Vec<Item>>) -> usize {
 }
 
 fn find_symmetry(pattern: &Pattern) -> usize {
-    let row_split_size = find_symmtery_one_dimension(&pattern.rows);
+    let row_split_size = find_symmtery_one_dimension(&pattern.rows, None);
     if row_split_size > 0 {
         return 100 * row_split_size;
     }
-    let col_split_size = find_symmtery_one_dimension(&pattern.columns);
+    let col_split_size = find_symmtery_one_dimension(&pattern.columns, None);
     if col_split_size > 0 {
         return col_split_size;
     }
     0
 }
 
-pub fn problem1() {
-    println!("problem 1");
+fn swapItem(item: Item) -> Item {
+    match item {
+        Item::Ash => Item::Rock,
+        Item::Rock => Item::Ash,
+    }
+}
 
+fn find_smudged_symmetry(pattern: &Pattern) -> usize {
+    let original_row_split_size = find_symmtery_one_dimension(&pattern.rows, None);
+    let original_col_split_size = find_symmtery_one_dimension(&pattern.columns, None);
+
+    let row_avoid = if original_row_split_size > 0 {
+        Some(original_row_split_size - 1)
+    } else {
+        None
+    };
+    let col_avoid = if original_col_split_size > 0 {
+        Some(original_col_split_size - 1)
+    } else {
+        None
+    };
+
+    for i in 0..pattern.rows.len() {
+        for j in 0..pattern.columns.len() {
+            let mut smudged_pattern: Pattern = pattern.clone();
+            smudged_pattern.rows[i][j] = swapItem(smudged_pattern.rows[i][j]);
+            smudged_pattern.columns[j][i] = swapItem(smudged_pattern.columns[j][i]);
+
+            assert_eq!(smudged_pattern.rows[i][j], smudged_pattern.columns[j][i]);
+
+            let row_split_size = find_symmtery_one_dimension(&smudged_pattern.rows, row_avoid);
+            if row_split_size > 0 {
+                return 100 * row_split_size;
+            }
+            let col_split_size = find_symmtery_one_dimension(&smudged_pattern.columns, col_avoid);
+            if col_split_size > 0 {
+                return col_split_size;
+            }
+        }
+    }
+    0
+}
+
+fn read_input() -> Vec<Pattern> {
     let path = Path::new("resources/day13_input");
     let contents = fs::read_to_string(path).unwrap();
 
@@ -109,7 +156,23 @@ pub fn problem1() {
         }
     });
 
+    patterns
+}
+
+pub fn problem1() {
+    println!("problem 1");
+
+    let patterns = read_input();
     let sum: usize = patterns.iter().map(|p| find_symmetry(p)).sum();
+
+    println!("Answer: {}", sum);
+}
+
+pub fn problem2() {
+    println!("problem 2");
+
+    let patterns = read_input();
+    let sum: usize = patterns.iter().map(|p| find_smudged_symmetry(p)).sum();
 
     println!("Answer: {}", sum);
 }
